291c171d69ed52807d9b97f6e1b20f3ba2fb123e
skyolson <54546783+skyolson@users.noreply.github.com>
2024-04-15 00:08:46 -0400

    Scaffolding for looped and nested queries, initial testing on login and running of queries

--- main.go
+++ main.go
@@ -24,13 +24,14 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
-	"os"
 	"reflect"
+	"slices"
+	"strings"
 
 	"encoding/json"
 
 	"github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/skysyzygy/tq/auth"
 	"github.com/skysyzygy/tq/client"
 )
@@ -40,13 +41,16 @@ import (
 // Global config options and structures for tq
 type tqConfig struct {
 	// the Tessitura API client
-	client.TessituraServiceWeb
+	*client.TessituraServiceWeb
 
 	// Basic auth for requests
-	basicAuth func(*runtime.ClientOperation)
+	basicAuth runtime.ClientAuthInfoWriter
 
-	// Bearer token for requests - not yet implemented
+	// TODO: Bearer token for requests
 	// tokenAuth func(*runtime.ClientOperation)
+
+	// TODO: Logging to file/stdout
+	// og io.WriteCloser
 
 	// some flags
 	verbose bool
@@ -56,48 +60,59 @@ type tqConfig struct {
 // Log in the Tessitura client with the given authentication info and cache the login data
 func (tq *tqConfig) Login(a auth.Auth) error {
 
-	if validLogin, err := a.Validate(); err != nil || !validLogin {
-		return errors.Join(fmt.Errorf("invalid login"), err)
-	}
 	// Cache the login data
 	if basicAuth, err := a.BasicAuth(); err != nil {
 		return err
 	} else {
 		tq.basicAuth = basicAuth
 	}
+
+	host := append(strings.SplitN(a.Hostname, "/", 2), "")
+	ignoreCerts, _ := httptransport.TLSClient(httptransport.TLSClientOptions{
+		InsecureSkipVerify: true,
+	})
+	transport := httptransport.NewWithClient(host[0], host[1], []string{"https"}, ignoreCerts)
+	transport.DefaultAuthentication = tq.basicAuth
+	tq.TessituraServiceWeb = client.New(transport, nil)
+
 	return nil
 }
 
-// Getter function for tq -- simply dispatches to `doGet` based on the type of thing we are querying.
-func (tq tqConfig) Get(thing string, query []byte) (res []byte, err error) {
-
-	if tq.verbose {
-		fmt.Printf("running Get for %v", thing)
+// Generic for doing operations (get/put/post), parallelizing calls to DoOne as needed
+// Returns the result, either a single json map if there is only one operation to do,
+// or an array of json maps if there are multiple operations.
+// Returns the last error
+func Do[P any, R any, O any, F func(*P, ...O) (R, error)](
+	tq tqConfig, function F, query []byte,
+) ([]byte, error) {
+	queries := new([]json.RawMessage)
+	err := json.Unmarshal(query, queries)
+	if _, ok := err.(*json.UnmarshalTypeError); ok {
+		// it's not an array... so just call DoOne
+		return DoOne(tq, function, query)
+	} else if err == nil {
+		// loop over queries and call DoOne for each
+		// TODO: Parallelize this!
+		out := []byte(`[`)
+		errs := make([]error, len(*queries))
+		var res []byte
+		for i, q := range *queries {
+			res, errs[i] = DoOne(tq, function, q)
+			out = slices.Concat(out, res)
+		}
+		out = slices.Concat(out, []byte(`]`))
+		return out, errs[len(errs)]
 	}
-
-	switch thing {
-	case "customers":
-		f := tq.TessituraServiceWeb.Get.ConstituentsGetConstituents
-		res, err = DoOne(tq, f, query)
-		return
-
-	case "emails":
-		f := tq.TessituraServiceWeb.Get.ElectronicAddressesGetAll
-		res, err = DoOne(tq, f, query)
-		return
-
-	case "addresses":
-		f := tq.TessituraServiceWeb.Get.AddressesGetAll
-		res, err = DoOne(tq, f, query)
-		return
-
-	default:
-		return nil, fmt.Errorf("no `thing` specified, nothing to do")
-	}
-
+	return nil, err // json Unmarshall error
 }
 
-// Generic for doing (one) thing (get/put/post)
+// Generic for doing operations (get/put/post), parallelizing calls to DoOne as needed
+// The type parameters allow it to work for any swagger-defined function.
+// It unmarshals `query` into the appropriate data structure for `function` and marshals
+// the result back to json.
+// If the initial unmarhshal fails, it tries again with a depth-first traversal of the
+// data structure, essentially trying to recast a flat query into the nested structure
+// required.
 func DoOne[P any, R any, O any, F func(*P, ...O) (R, error)](
 	tq tqConfig, function F, query []byte,
 ) ([]byte, error) {
@@ -105,22 +120,31 @@ func DoOne[P any, R any, O any, F func(*P, ...O) (R, error)](
 	params := new(P)
 	remainder, err := unmarshallStructWithRemainder(query, params)
 
+	// Is there a better way to determine failure?
+	if len(structFields(*params)) == 1 && len(mapFields(remainder)) > 0 {
+		remainder, err = unmarshallNestedStructWithRemainder(query, params)
+	}
+
 	if tq.verbose {
-		unmarshallStructReport(params, remainder, os.Stdout)
+		structFields(*params)
+		mapFields(remainder)
 	}
 	if tq.dryRun || err != nil {
 		return nil, err
 	}
 
 	// Call the function with basic authentication
-	obj, err := function(params, interface{}(tq.basicAuth).(O))
+	obj, err := function(params)
 
-	// Marshall the json response
-	res, err := json.Marshal(obj)
 	if err != nil {
 		return nil, err
 	} else {
-		return res, nil
+		// Marshall the json response
+		if payload := reflect.ValueOf(obj).Elem().FieldByName("Payload"); !payload.IsZero() {
+			return json.Marshal(payload.Interface())
+		} else {
+			return json.Marshal(obj)
+		}
 	}
 }
 
@@ -131,9 +155,11 @@ func main() {
 
 // Unmarshall into a struct and return the remainder as a map
 // Errors if P is not a struct type
-func unmarshallStructWithRemainder[P any](query []byte, params *P) (res map[string]any, err error) {
-	if reflect.TypeOf(*params).Kind() != reflect.Struct {
-		return nil, fmt.Errorf("params must be struct, got %v", reflect.TypeOf(*params).Kind())
+func unmarshallStructWithRemainder(query []byte, params any) (res map[string]any, err error) {
+	if reflect.TypeOf(params).Kind() != reflect.Pointer {
+		return nil, fmt.Errorf("params must be pointer to struct, got %v", reflect.TypeOf(params).Kind())
+	} else if reflect.TypeOf(params).Elem().Kind() != reflect.Struct {
+		return nil, fmt.Errorf("params must be pointer to struct, got pointer to %v", reflect.TypeOf(params).Elem().Kind())
 	}
 
 	// Unmarshal the query into the given parameter structure
@@ -145,7 +171,7 @@ func unmarshallStructWithRemainder[P any](query []byte, params *P) (res map[stri
 	err = errors.Join(err, _err)
 
 	// Remove keys that are in the struct already
-	typ := reflect.TypeOf(*params)
+	typ := reflect.TypeOf(params).Elem()
 	for key := range res {
 		if _, ok := typ.FieldByName(key); ok {
 			delete(res, key)
@@ -155,8 +181,35 @@ func unmarshallStructWithRemainder[P any](query []byte, params *P) (res map[stri
 	return
 }
 
-// Print a message about the different keys in the struct `s` and map `m`
-func unmarshallStructReport(s any, m map[string]any, stdout io.WriteCloser) {
+// Unmarshall into a nested struct from a flat query and return the remainder as a map
+// Errors if P is not a struct type
+func unmarshallNestedStructWithRemainder(query []byte, params any) (res map[string]any, err error) {
+	if reflect.TypeOf(params).Kind() != reflect.Pointer {
+		return nil, fmt.Errorf("params must be pointer to struct, got %v", reflect.TypeOf(params).Kind())
+	} else if reflect.TypeOf(params).Elem().Kind() != reflect.Struct {
+		return nil, fmt.Errorf("params must be pointer to struct, got pointer to %v", reflect.TypeOf(params).Elem().Kind())
+	}
+
+	v := reflect.ValueOf(params).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).Kind() == reflect.Struct && v.Field(i).IsZero() {
+			// Recurse if there's a struct field
+			res, err = unmarshallNestedStructWithRemainder(query, v.Field(i).Addr().Interface())
+			if err != nil {
+				return
+			}
+			// Update the query with only unmatched fields
+			query, _ = json.Marshal(res)
+		}
+	}
+
+	res, err = unmarshallStructWithRemainder(query, params)
+
+	return
+}
+
+// Return the field names from struct `s` that are not zero/nil
+func structFields(s any) []string {
 	typ := reflect.TypeOf(s)
 	val := reflect.ValueOf(s)
 	if typ.Kind() == reflect.Pointer {
@@ -169,8 +222,14 @@ func unmarshallStructReport(s any, m map[string]any, stdout io.WriteCloser) {
 			usedFields = append(usedFields, typ.Field(i).Name)
 		}
 	}
-	// Get the fields that marshmallow didn't map
-	mapFields := reflect.ValueOf(m).MapKeys()
-	fmt.Fprintf(stdout, "executing a query using fields %v and ignoring fields %v", usedFields, mapFields)
-	stdout.Close()
+	return usedFields
+}
+
+// Return the field names from map `m`
+func mapFields(m map[string]any) []string {
+	fields := make([]string, len(m))
+	for field := range m {
+		fields = append(fields, field)
+	}
+	return fields
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
-	"os"
-	"regexp"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/skysyzygy/tq/auth"
+	"github.com/skysyzygy/tq/client/g_e_t"
+	"github.com/skysyzygy/tq/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,7 +35,7 @@ func Test_unmarshallStructWithRemainder(t *testing.T) {
 	q := new(Q)
 	res, err = unmarshallStructWithRemainder([]byte(`{"A": "these", "B": "are", "C": "words"}`), q)
 	assert.Equal(t, Q(nil), *q)
-	assert.ErrorContains(t, err, "must be struct")
+	assert.ErrorContains(t, err, "must be pointer to struct")
 	assert.Equal(t, Q(nil), Q(res))
 
 	// test that unmarshallWithRemainder returns an error if it's given a JSON array
@@ -41,30 +46,82 @@ func Test_unmarshallStructWithRemainder(t *testing.T) {
 
 }
 
-// test that unmarshalLStructReport returns used and unused element of struct and map for pointer, string, and int types
-func Test_unmarshallStructReport(t *testing.T) {
+func Test_unmarshallNestedStructWithRemainder(t *testing.T) {
+	type P struct{ A, B, C string }
+	type N struct {
+		D, E, F string
+		Nest    P
+	}
+
+	// test that unmarshallNestedWithRemainder fills nested struct and returns map of extra data
+	n := new(N)
+	res, err := unmarshallNestedStructWithRemainder([]byte(`{"A": "these", "B": "are", "C": "words"}`), n)
+	assert.Equal(t, N{Nest: P{"these", "are", "words"}}, *n)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(res))
+
+	n = new(N)
+	res, err = unmarshallNestedStructWithRemainder([]byte(`{"A": "these", "B": "are", "C": "words", "D": "nothing", "E": "more"}`), n)
+	assert.Equal(t, N{D: "nothing", E: "more", Nest: P{"these", "are", "words"}}, *n)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(res))
+
+	n = new(N)
+	res, err = unmarshallNestedStructWithRemainder([]byte(`{"A": "these", "B": "are", "C": "words", "D": "nothing", "Z": "zzz"}`), n)
+	assert.Equal(t, N{D: "nothing", Nest: P{"these", "are", "words"}}, *n)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(res))
+	assert.Equal(t, map[string]any{"Z": "zzz"}, res)
+
+}
+
+// test that structFields returns used elements of struct pointer, string, and int types
+func Test_structFields(t *testing.T) {
 
 	strings := []string{"these", "are", "words"}
 	pp := struct{ A, B, C, empty *string }{&strings[0], &strings[1], &strings[2], nil}
-	q := map[string]any{"D": "nothing", "E": "more"}
-
-	r, w, _ := os.Pipe()
-	unmarshallStructReport(pp, q, w)
-	output, err := io.ReadAll(r)
-	assert.Regexp(t, regexp.MustCompile(`executing a query using fields \[[ABC ]+\] and ignoring fields \[[DE ]+\]`), string(output))
-	assert.NoError(t, err)
-
 	ps := struct{ A, B, C, empty string }{strings[0], strings[1], strings[2], ""}
-	r, w, _ = os.Pipe()
-	unmarshallStructReport(ps, q, w)
-	output, err = io.ReadAll(r)
-	assert.Regexp(t, regexp.MustCompile(`executing a query using fields \[[ABC ]+\] and ignoring fields \[[DE ]+\]`), string(output))
+	pi := struct{ A, B, C, empty int }{1, 2, 3, 0}
+
+	assert.Equal(t, []string{"A", "B", "C"}, structFields(pp))
+	assert.Equal(t, []string{"A", "B", "C"}, structFields(ps))
+	assert.Equal(t, []string{"A", "B", "C"}, structFields(pi))
+
+}
+
+// test that Login builds Tessitura API client and saves BasicAuth info for future use
+func Test_Login(t *testing.T) {
+	tq := new(tqConfig)
+	tq.Login(auth.New("hostname", "user", "", "", nil))
+	assert.NotNil(t, tq.Get)
+	assert.NotNil(t, tq.basicAuth)
+}
+
+// test that DoOne calls a swagger API function and returns a response
+func Test_DoOne(t *testing.T) {
+	oneConstituent := models.Constituent{
+		ID:        0,
+		FirstName: "Test",
+		LastName:  "User",
+	}
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		req := g_e_t.ConstituentsGetConstituentParams{}
+
+		reqBody, _ := io.ReadAll(r.Body)
+		json.Unmarshal(reqBody, &req)
+
+		resBody, _ := json.Marshal(oneConstituent)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(resBody)
+	}))
+	defer server.Close()
+	tq := new(tqConfig)
+	query := []byte(`{"Id": 0}`)
+	tq.Login(auth.New(strings.Replace(server.URL, "https://", "", 1), "user", "", "", nil))
+
+	res, err := DoOne(*tq, tq.Get.ConstituentsGet, query)
+	expectedJSON, _ := json.Marshal(oneConstituent)
+	assert.Equal(t, expectedJSON, res)
 	assert.NoError(t, err)
 
-	pi := struct{ A, B, C, empty int }{1, 2, 3, 0}
-	r, w, _ = os.Pipe()
-	unmarshallStructReport(pi, q, w)
-	output, err = io.ReadAll(r)
-	assert.Regexp(t, regexp.MustCompile(`executing a query using fields \[[ABC ]+\] and ignoring fields \[[DE ]+\]`), string(output))
-	assert.NoError(t, err)
 }
