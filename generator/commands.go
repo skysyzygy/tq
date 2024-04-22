5c58b59ee342238b43690db0c77269518527b725
skyolson <54546783+skyolson@users.noreply.github.com>
2024-04-21 22:52:03 -0400

    instantiateStructType / usage now generate usage info up to the first level of incoming swagger structs. newCommand does some text wrangling to figure out the verb/thing/variant the command is operating on ... almost ready for command generation!

--- generator/commands.go
+++ generator/commands.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"embed"
+	"encoding/json"
 	"fmt"
-	"go/doc"
-	"go/parser"
-	"go/token"
-	"regexp"
+	"reflect"
+	"slices"
 	"strings"
 
 	"github.com/go-openapi/spec"
@@ -29,36 +28,11 @@ func init() {
 	spec.ExpandSpec(swagger, nil)
 }
 
-func DescribeFunc(funcName string) (short string, long string) {
-	paths := []string{"g_e_t", "p_u_t", "p_o_s_t", "d_e_l_e_t_e", "h_e_a_d"}
-	for _, path := range paths {
-		re := regexp.MustCompile(`\s*` + funcName + `\s*`)
-		pkgs, _ := parser.ParseDir(token.NewFileSet(), "../client/"+path, nil, parser.ParseComments)
-		for _, pkg := range pkgs {
-			doc := doc.New(pkg, "../client", 0)
-			for _, typ := range doc.Types {
-				if typ.Name == "Client" {
-					for _, fun := range typ.Methods {
-						if fun.Name == funcName {
-							short = fun.Name
-							long = re.ReplaceAllString(fun.Doc, "")
-							return
-						}
-					}
-
-				}
-			}
-		}
-
-	}
-	return
-}
-
 // Reads the Tessitura swagger spec and extracts documentation info
 func Describe(funcName string) (short string, long string) {
 
 	// now find the corresponding operation in the swagger spec!
-	for _, path := range swagger.Paths.Paths {
+	for url, path := range swagger.Paths.Paths {
 		for _, op := range []*spec.Operation{
 			path.Get, path.Put, path.Post, path.Delete, path.Head,
 		} {
@@ -68,9 +42,12 @@ func Describe(funcName string) (short string, long string) {
 			if op != nil && funcName ==
 				// the swagger doc has an extra "_" in the name
 				strings.ReplaceAll(op.ID, "_", "") {
-				short = funcName
+				url := strings.Split(url, "/")
+				short = url[1]
+				if len(url) > 2 {
+					short = url[2]
+				}
 				long = op.Summary
-
 				for _, param := range op.OperationProps.Parameters {
 					paramReq := ""
 					if param.Required {
@@ -92,4 +69,98 @@ func Describe(funcName string) (short string, long string) {
 	}
 
 	return
+}
+
+// Instantiate a struct type and fill it with descriptions up to depth (starting at 0)
+func instantiateStructType(t reflect.Type, depth int) any {
+	var v reflect.Value
+
+	// instantiate the root element
+	if t.Kind() == reflect.Pointer &&
+		t.Elem().Kind() == reflect.Struct {
+		// v = *new(*t)
+		v = reflect.New(t.Elem()).Elem()
+	} else if t.Kind() == reflect.Struct {
+		// v = *new(t)
+		v = reflect.New(t).Elem()
+	} else {
+		return reflect.New(t).Interface()
+	}
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if field.CanSet() {
+			if field.Kind() == reflect.Pointer {
+				field.Set(reflect.New(field.Type().Elem()))
+				field = field.Elem()
+			}
+			switch field.Kind() {
+			// bools, ints and floats are already initialized
+			case reflect.String:
+				field.SetString("string")
+			case reflect.Struct:
+				if depth > 0 {
+					field.Set(reflect.ValueOf(instantiateStructType(field.Type(), depth-1)))
+				} else {
+					// unset pointer
+					v.Field(i).SetZero()
+				}
+			}
+		}
+	}
+
+	if t.Kind() == reflect.Pointer {
+		return v.Addr().Interface()
+	}
+	return v.Interface()
+}
+
+func usage(method reflect.Value) []byte {
+	params := reflect.ValueOf(instantiateStructType(method.Type().In(1), 1))
+	if params.Kind() == reflect.Pointer {
+		params = params.Elem()
+	}
+	if params.Kind() != reflect.Struct {
+		// Oops! Don't know what to do =)
+		return nil
+	}
+
+	paramsMap := make(map[string]any)
+	removeFields := []string{"timeout", "Context", "HTTPClient"}
+	for i := 0; i < params.NumField(); i++ {
+		fieldName := params.Type().Field(i).Name
+		if !slices.Contains(removeFields, fieldName) {
+			paramsMap[fieldName] = params.Field(i).Interface()
+		}
+	}
+
+	usage, err := json.Marshal(paramsMap)
+	if err != nil {
+		panic(err)
+	}
+	usage = []byte(strings.ReplaceAll(string(usage), "null", "[object]"))
+	return usage
+}
+
+// Fills a command struct with info about a method
+func newCommand(method reflect.Method) command {
+	short, long := Describe(method.Name)
+	stopWords := []string{"Get", "Update", "Create", "Delete", short}
+	variant := method.Name
+	verb := ""
+	for _, stopWord := range stopWords {
+		if strings.Contains(variant, stopWord) && verb == "" {
+			verb = stopWord
+		}
+		variant = strings.ReplaceAll(variant, stopWord, "")
+	}
+	return command{
+		verb:    verb,
+		thing:   short,
+		variant: variant,
+		long:    long,
+		usage:   string(usage(method.Func)),
+		aliases: []string{short},
+	}
+
 }
--- generator/commands_test.go
+++ generator/commands_test.go
@@ -1,65 +1,137 @@
 package main
 
 import (
+	"encoding/json"
+	"reflect"
+	"slices"
 	"testing"
 
+	"github.com/skysyzygy/tq/client"
+	"github.com/skysyzygy/tq/client/g_e_t"
+	"github.com/skysyzygy/tq/client/p_o_s_t"
+	"github.com/skysyzygy/tq/client/p_u_t"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_Describe(t *testing.T) {
 
 	short, long := Describe("ConstituentsGet")
-	assert.Equal(t, "ConstituentsGet", short)
+	assert.Equal(t, "Constituents", short)
 	assert.Equal(t, "Get the details of a Constituent using id.\n ConstituentId(*): string\t", long)
 
 	short, long = Describe("ConstituentsUpdate")
-	assert.Equal(t, "ConstituentsUpdate", short)
+	assert.Equal(t, "Constituents", short)
 	assert.Equal(t, "Update a constituent. Only the information about constituent can be updated. If addresses, electronicAddresses, salutations or phones data are passed, they will be ignored.\n ConstituentId(*): string\t\n Constituent(*): object\t", long)
 
 	short, long = Describe("ConstituentsCreateConstituent")
-	assert.Equal(t, "ConstituentsCreateConstituent", short)
+	assert.Equal(t, "Constituents", short)
 	assert.Equal(t, "Create a new constituent with addresses, electronicAddresses, salutations and phones.\n Constituent(*): object\t", long)
 
 	short, long = Describe("ElectronicAddressesGet")
-	assert.Equal(t, "ElectronicAddressesGet", short)
+	assert.Equal(t, "ElectronicAddresses", short)
 	assert.Equal(t, "Get details of an electronic address.\n ElectronicAddressId(*): string\t", long)
 
 	short, long = Describe("ElectronicAddressesUpdate")
-	assert.Equal(t, "ElectronicAddressesUpdate", short)
+	assert.Equal(t, "ElectronicAddresses", short)
 	assert.Equal(t, "Update an electronic address.\n ElectronicAddressId(*): string\t\n ElectronicAddress(*): object\t", long)
 
 	short, long = Describe("ElectronicAddressesCreate")
-	assert.Equal(t, "ElectronicAddressesCreate", short)
+	assert.Equal(t, "ElectronicAddresses", short)
 	assert.Equal(t, "Create a new electronic address.\n ElectronicAddress(*): object\t", long)
 
 	short, long = Describe("AssociationTypesGet")
-	assert.Equal(t, "AssociationTypesGet", short)
+	assert.Equal(t, "AssociationTypes", short)
 	assert.Equal(t, "Get the details of an association type by id. To get the resource only if the user has write/edit access, pass filter=\"writeonly\". To get the resources in maintenance mode(ignore Control Groups), pass maintenanceMode=\"true\". Maintenance mode requires users to have access to the reference tables. (Specified in TX_REFTABLE_USERGROUP).\n Id(*): string\t\n Filter: string\tFilter by user access (default: readwrite)\n MaintenanceMode: string\tIgnore control grouping (default: false)", long)
 
 }
 
-func Test_DescribeFunc(t *testing.T) {
-	short, long := DescribeFunc("ConstituentsGet")
-	assert.Equal(t, "ConstituentsGet", short)
-	assert.Equal(t, "gets the details of a constituent using id\n", long)
+// Test that instantiateStructType recurses into a struct, navigating pointers, and initializes
+// strings with "string" and everything else with zero type
+func Test_instantiateStructType(t *testing.T) {
+	type test struct {
+		I  int
+		F  float32
+		B  bool
+		S  string
+		SP *string
+	}
+	type nest struct {
+		P *test
+		O test
+	}
+	pointer := new(nest)
+	object := *pointer
+	j, _ := json.Marshal(instantiateStructType(reflect.TypeOf(object), 1))
+	assert.Equal(t,
+		`{"P":{"I":0,"F":0,"B":false,"S":"string","SP":"string"},"O":{"I":0,"F":0,"B":false,"S":"string","SP":"string"}}`,
+		string(j))
 
-	short, long = DescribeFunc("ConstituentsUpdate")
-	assert.Equal(t, "ConstituentsUpdate", short)
-	assert.Equal(t, "updates a constituent only the information about constituent can be updated if addresses electronic addresses salutations or phones data are passed they will be ignored\n", long)
+	j, _ = json.Marshal(instantiateStructType(reflect.TypeOf(pointer), 1))
+	assert.Equal(t,
+		`{"P":{"I":0,"F":0,"B":false,"S":"string","SP":"string"},"O":{"I":0,"F":0,"B":false,"S":"string","SP":"string"}}`,
+		string(j))
 
-	short, long = DescribeFunc("ConstituentsCreateConstituent")
-	assert.Equal(t, "ConstituentsCreateConstituent", short)
-	assert.Equal(t, "creates a new constituent with addresses electronic addresses salutations and phones\n", long)
+	j, _ = json.Marshal(instantiateStructType(reflect.TypeOf(pointer), 0))
+	assert.Equal(t,
+		`{"P":null,"O":{"I":0,"F":0,"B":false,"S":"","SP":null}}`,
+		string(j))
 
-	short, long = DescribeFunc("ElectronicAddressesGet")
-	assert.Equal(t, "ElectronicAddressesGet", short)
-	assert.Equal(t, "gets details of an electronic address\n", long)
+}
 
-	short, long = DescribeFunc("ElectronicAddressesUpdate")
-	assert.Equal(t, "ElectronicAddressesUpdate", short)
-	assert.Equal(t, "updates an electronic address\n", long)
+func Test_usage(t *testing.T) {
+	assert.Equal(t, `{"ConstituentID":"string"}`,
+		string(usage(reflect.ValueOf(g_e_t.ClientService.ConstituentsGet))))
+	assert.Equal(t, `{"Constituent":{"CreateLocation":"string","CreatedBy":"string","CreatedDateTime":"0001-01-01T00:00:00.000Z","DisplayName":"string","FirstName":"string","LastActivityDate":"0001-01-01T00:00:00.000Z","LastGiftDate":"0001-01-01T00:00:00.000Z","LastName":"string","LastTicketDate":"0001-01-01T00:00:00.000Z","MiddleName":"string","SortName":"string","UpdatedBy":"string","UpdatedDateTime":"0001-01-01T00:00:00.000Z"},"ConstituentID":"string"}`,
+		string(usage(reflect.ValueOf(p_u_t.ClientService.ConstituentsUpdate))))
+	assert.Equal(t, `{"Constituent":{"Addresses":[object],"CreateLocation":"string","CreatedBy":"string","CreatedDateTime":"0001-01-01T00:00:00.000Z","DisplayName":"string","ElectronicAddresses":[object],"FirstName":"string","LastActivityDate":"0001-01-01T00:00:00.000Z","LastGiftDate":"0001-01-01T00:00:00.000Z","LastName":"string","LastTicketDate":"0001-01-01T00:00:00.000Z","MiddleName":"string","PhoneNumbers":[object],"Salutations":[object],"SortName":"string","UpdatedBy":"string","UpdatedDateTime":"0001-01-01T00:00:00.000Z"}}`,
+		string(usage(reflect.ValueOf(p_o_s_t.ClientService.ConstituentsCreateConstituent))))
+}
 
-	short, long = DescribeFunc("ElectronicAddressesCreate")
-	assert.Equal(t, "ElectronicAddressesCreate", short)
-	assert.Equal(t, "creates a new electronic address\n", long)
+func Test_newCommandGet(t *testing.T) {
+	client := client.New(nil, nil)
+	c := reflect.TypeOf(client.Get)
+	commands := make([]command, c.NumMethod())
+	for i := 0; i < c.NumMethod(); i++ {
+		if methodName := c.Method(i).Name; slices.Contains([]string{
+			"SetTransport", "CacheInit", "DiagnosticsGetAPIPluginConfiguration"},
+			methodName) {
+			continue
+		}
+		commands[i] = newCommand(c.Method(i))
+		assert.NotEqual(t, "", commands[i].verb, commands[i])
+		assert.NotEqual(t, "", commands[i].thing, commands[i])
+	}
+}
+
+func Test_newCommandPut(t *testing.T) {
+	client := client.New(nil, nil)
+	c := reflect.TypeOf(client.Put)
+	commands := make([]command, c.NumMethod())
+	for i := 0; i < c.NumMethod(); i++ {
+		if methodName := c.Method(i).Name; slices.Contains([]string{
+			"SetTransport", "DiagnosticsPut"},
+			methodName) {
+			continue
+		}
+		commands[i] = newCommand(c.Method(i))
+		assert.NotEqual(t, "", commands[i].verb, commands[i])
+		assert.NotEqual(t, "", commands[i].thing, commands[i])
+	}
+}
+
+func Test_newCommandPost(t *testing.T) {
+	client := client.New(nil, nil)
+	c := reflect.TypeOf(client.Post)
+	commands := make([]command, c.NumMethod())
+	for i := 0; i < c.NumMethod(); i++ {
+		if methodName := c.Method(i).Name; slices.Contains([]string{
+			"SetTransport",
+			"EmailsSendTickets", "OrdersPrintHTMLTickets"},
+			methodName) {
+			continue
+		}
+		commands[i] = newCommand(c.Method(i))
+		assert.NotEqual(t, "", commands[i].verb, commands[i])
+		assert.NotEqual(t, "", commands[i].thing, commands[i])
+	}
 }
