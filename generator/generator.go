12348328ff618cf9a089aa302e7c825fcd7f7dfc
skysyzygy <sky@skysyzygy.xyz>
2024-04-27 10:33:54 -0400

    Added tests for generator

--- generator/generator.go
+++ generator/generator.go
@@ -12,46 +12,58 @@ import (
 )
 
 func main() {
-	tmpl, err := template.ParseFiles("commands.go.tmpl")
-	if err != nil {
-		panic(err)
-	}
-
 	for _, op := range []string{"Get", "Put", "Post"} {
-		outFile, err := os.Create("../cmd/" + strings.ToLower(op) + ".go")
-		if err != nil {
-			panic(err)
-		}
-
-		client := client.New(nil, nil)
-		doer, ok := reflect.TypeOf(*client).FieldByName(op)
-		if !ok {
-			panic(fmt.Errorf("couldn't get client.%v", op))
-		}
-
-		// Group commands by the thing they operate on
-		commands := make(map[string][]command)
-		for i := 0; i < doer.Type.NumMethod(); i++ {
-			command := newCommand(doer.Type.Method(i))
-			if command.Thing != "" {
-				commands[command.Thing] = append(commands[command.Thing], command)
-			}
-		}
-
-		// Ensure that the first command is the one without a variant
-		for _, commands := range commands {
-			slices.SortFunc(commands, func(a command, b command) int {
-				return strings.Compare(a.Variant, b.Variant)
-			})
-		}
-
-		data := make(map[string]any)
-		data["op"] = op
-		data["commands"] = commands
-		data["makeAliases"] = makeAliases
-		err = tmpl.Execute(outFile, data)
-		if err != nil {
+		if err := execTemplate("commands.go.tmpl", "../cmd/"+strings.ToLower(op)+".go", getDataForOperation(op)); err != nil {
 			panic(err)
 		}
 	}
+}
+
+// Run the template in `inFilename` using `data` and save as `outFilename`
+func execTemplate(inFilename string, outFilename string, data any) error {
+	tmpl, err := template.ParseFiles(inFilename)
+	if err != nil {
+		return err
+	}
+	outFile, err := os.Create(outFilename)
+
+	if err != nil {
+		return err
+	}
+	err = tmpl.Execute(outFile, data)
+	if err != nil {
+		return err
+	}
+	return outFile.Close()
+}
+
+// Build data about entities that can be used with `operation` (i.e. "Get", "Post", "Put")
+func getDataForOperation(operation string) (data map[string]any) {
+	client := client.New(nil, nil)
+	doer, ok := reflect.TypeOf(*client).FieldByName(operation)
+	if !ok {
+		panic(fmt.Errorf("couldn't get client.%v", operation))
+	}
+
+	// Group commands by the thing they operate on
+	commands := make(map[string][]command)
+	for i := 0; i < doer.Type.NumMethod(); i++ {
+		command := newCommand(doer.Type.Method(i))
+		if command.Thing != "" {
+			commands[command.Thing] = append(commands[command.Thing], command)
+		}
+	}
+
+	// Ensure that the first command is the one without a variant
+	for _, commands := range commands {
+		slices.SortFunc(commands, func(a command, b command) int {
+			return strings.Compare(a.Variant, b.Variant)
+		})
+	}
+
+	data = make(map[string]any)
+	data["op"] = operation
+	data["commands"] = commands
+	data["makeAliases"] = makeAliases
+	return
 }
--- generator/generator_test.go
+++ generator/generator_test.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_execTemplate(t *testing.T) {
+	data := map[string]string{"world": "Yourself!"}
+	filename := "execTemplate.txt"
+	assert.NoFileExists(t, filename)
+
+	// Execute the template
+	execTemplate("generator_test.tmpl", filename, data)
+	assert.FileExists(t, filename)
+
+	// Read the output
+	contents, _ := os.ReadFile(filename)
+	assert.Equal(t, "Hello Yourself!", string(contents))
+
+	// Clean up
+	os.Remove(filename)
+	assert.NoFileExists(t, filename)
+}
+
+func Test_getDataForOperation(t *testing.T) {
+	assert.Panics(t, func() { getDataForOperation("NotAnOp") })
+
+	get := getDataForOperation("Get")
+	assert.Equal(t, get["op"], "Get")
+	commands, ok := get["commands"].(map[string][]command)
+	assert.True(t, ok, "get[`commands`] is a map of slices of `command`s")
+	assert.Greater(t, len(commands), 100)
+
+	ops := make(map[string]any)
+	for _, op := range []string{"Get", "Put", "Post"} {
+		ops[op] = getDataForOperation(op)
+	}
+
+	commandHash := make(map[string]bool)
+	// Check that there are no matching verb/thing/variant combos
+	// and that there is alignment between the data map and command structs
+	for op, data := range ops {
+		things := data.(map[string]any)["commands"].(map[string][]command)
+		for thing, commands := range things {
+			for i, command := range commands {
+				//assert.Equal(t, op, command.Verb) -- verbs are sometimes not the same as the op (e.g. "Create", "Update")
+				assert.Equal(t, thing, command.Thing)
+				assert.False(t, commandHash[strings.Join([]string{op, thing, command.Variant}, "__")])
+				commandHash[strings.Join([]string{op, thing, command.Variant}, "__")] = true
+
+				// ensure that only the first command in a slice can be without variant
+				if i != 0 {
+					assert.NotEqual(t, "", command.Variant)
+				}
+
+			}
+		}
+	}
+
+}
