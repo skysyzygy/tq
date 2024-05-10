ca5270d362f3ae4203a59b986aae702209f83aeb
skyolson <54546783+skyolson@users.noreply.github.com>
2024-05-09 23:13:11 -0400

    Return 204/NoContent for certain api urls as set in the swagger spec

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -35,6 +36,45 @@ func TestMain(m *testing.M) {
 }
 
 func server() (server *httptest.Server) {
+	methods := map[string]string{
+		"/Web/Cart/.+?/Bookings":                         "POST",
+		"/Web/Cart/.+?/Contributions/.+?/CustomData/.+?": "PUT",
+		"/Web/Cart/.+?/CustomData/.+?":                   "PUT",
+		"/TXN/PerformancePriceTypes":                     "PUT",
+		"/Cache/init":                                    "GET",
+		"/Diagnostics/Ping":                              "HEAD",
+		"/TXN/EmailResponses":                            "POST",
+		"/TXN/Performances/Copy":                         "POST",
+		"/TXN/Performances/Reschedule":                   "POST",
+		"/TXN/BulkCopySets/.+?/CopyDay":                  "POST",
+		"/Web/Session/.+?/Logout":                        "POST",
+		"/Web/Session/.+?/Transfer":                      "POST",
+		"/Web/Session/.+?/Reprint":                       "POST",
+		"/Web/Cart/.+?/Tickets/Return":                   "POST",
+		"/Web/Cart/.+?/Tickets/ReturnWithSeat":           "POST",
+		"/Web/Cart/.+?/Price":                            "POST",
+		"/Web/Cart/.+?/ValidateLimits":                   "POST",
+		"/Emails/Send":                                   "POST",
+		"/Emails/OrderConfirmation/.+?/Send":             "POST",
+		"/Emails/LoginCredentials/.+?/Send":              "POST",
+		"/Emails/ConstituentInfo/.+?/Send":               "POST",
+		"/Emails/Orders/.+?/Tickets/Send":                "POST",
+		"/TXN/PerformancePriceLayers/Summaries":          "POST",
+		"/TXN/PriceEvents/MoveTo":                        "PUT",
+		"/Web/Cart/.+?/LineItems/.+?/PriceTypeId":        "PUT",
+		"/Web/Cart/.+?/SubLineItems/.+?/PriceTypeId":     "PUT",
+		"/Web/Cart/.+?/SubLineItems/.+?/Recipient":       "PUT",
+		"/Web/Cart/.+?/LineItems/.+?/SpecialRequest":     "PUT",
+		"/Reporting/ReportRequests/FlushIncomplete":      "PUT",
+		"/TXN/PerformancePriceTypes/Base":                "PUT",
+		"/TXN/PerformancePriceLayers/Prices":             "PUT",
+	}
+
+	regexes := make(map[string]*regexp.Regexp)
+	for url := range methods {
+		regexes[url] = regexp.MustCompile(url)
+	}
+
 	server = httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var resBody []byte
 		w.Header().Set("Content-Type", "application/json")
@@ -45,6 +85,12 @@ func server() (server *httptest.Server) {
 				Message:         "",
 			}
 			resBody, _ = json.Marshal(&res)
+		}
+
+		for url, regex := range regexes {
+			if regex.MatchString(r.RequestURI) && r.Method == methods[url] {
+				w.WriteHeader(204)
+			}
 		}
 
 		w.Write(resBody)
