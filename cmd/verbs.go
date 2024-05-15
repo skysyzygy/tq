429d71219f7040f29dbd819bb85ba387cbbe6da5
skyolson <54546783+skyolson@users.noreply.github.com>
2024-05-14 22:40:09 -0400

    Added some help text about array syntax

--- cmd/verbs.go
+++ cmd/verbs.go
@@ -4,6 +4,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const longSuffix string = `Queries are simply JSON objects and can be batched by ` +
+	`combining multiple query objects into a single JSON array, e.g.\n` +
+	`[{"ID":123},{"ID":124},{"ID":125},...]` +
+	`Object definitions are detailed in the help for each command.`
+
 var Get_cmd = &cobra.Command{
 	Use:     "get",
 	Aliases: []string{"Get", "get", "Retrieve", "retrieve", "G", "g", "R", "r"},
@@ -23,5 +28,8 @@ var Put_cmd = &cobra.Command{
 }
 
 func init() {
+	Get_cmd.Long = Get_cmd.Short + "\n" + longSuffix
+	Post_cmd.Long = Post_cmd.Short + "\n" + longSuffix
+	Put_cmd.Long = Put_cmd.Short + "\n" + longSuffix
 	rootCmd.AddCommand(Get_cmd, Post_cmd, Put_cmd)
 }
