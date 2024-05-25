1fb567a429907d24de4fcdd3dea55a64570899bf
skyolson <54546783+skyolson@users.noreply.github.com>
2024-05-25 10:25:23 -0400

    Add fallback JSON unmarshaling and text unmarshaling

--- models/error_message_unmarshaler.go
+++ models/error_message_unmarshaler.go
@@ -13,10 +13,18 @@ func (m *ErrorMessage) UnmarshalJSON(b []byte) error {
 	err := swag.ReadJSON(b, &res)
 	if err != nil {
 		err = json.Unmarshal(b, &res[0])
-		if err != nil {
-			return err
-		}
+	}
+	if err != nil {
+		res[0].Code = "500"
+		res[0].Description = string(b)
+		res[0].Details = "Unknown Error Message"
 	}
 	*m = ErrorMessage(res[0])
+	return nil
+}
+
+// UnmarshalText interface implementation
+func (m *ErrorMessage) UnmarshalText(b []byte) error {
+	m.Description = string(b)
 	return nil
 }
