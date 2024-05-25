042f363e396d295be1390af4b05562aa6f158b88
skyolson <54546783+skyolson@users.noreply.github.com>
2024-05-25 10:24:24 -0400

    Oops get rid of test code

--- cmd/root.go
+++ cmd/root.go
@@ -62,7 +62,6 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	rootCmd.SetArgs([]string{"--version"})
 	err := rootCmd.Execute()
 	if err != nil {
 		if _tq != nil && _tq.Log != nil {
