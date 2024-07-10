461989ae1d4163de22b0c9f1d38ec34bad5c2bd0
skyolson <54546783+skyolson@users.noreply.github.com>
2024-07-10 18:50:03 -0400

    Remove unused styles

--- cmd/styles.go
+++ cmd/styles.go
@@ -3,12 +3,12 @@ package cmd
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	width     = 80
-	subtle    = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
-	highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
-	success   = lipgloss.AdaptiveColor{Light: "#43BF6D", Dark: "#73F59F"}
+	width  = 80
+	subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+	// highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	// success   = lipgloss.AdaptiveColor{Light: "#43BF6D", Dark: "#73F59F"}
 
-	successStyle = lipgloss.NewStyle().Foreground(success)
+	// successStyle = lipgloss.NewStyle().Foreground(success)
 
 	paraStyle = lipgloss.NewStyle().
 			Align(lipgloss.Left).
@@ -16,12 +16,12 @@ var (
 		//Background(highlight).
 		Padding(1, 2)
 
-	titleStyle = lipgloss.NewStyle().
-			MarginLeft(1).
-			MarginRight(5).
-			Padding(0, 1).
-			Italic(true) //.
-		//Foreground(highlight)
+	// titleStyle = lipgloss.NewStyle().
+	// 		MarginLeft(1).
+	// 		MarginRight(5).
+	// 		Padding(0, 1).
+	// 		Italic(true) //.
+	// 	//Foreground(highlight)
 
 	hiliteStyle = lipgloss.NewStyle().
 		//Foreground(lipgloss.Color("#FFFDF5")).
